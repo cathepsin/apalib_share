16384bdb9dcfad54e07915ab8ac94b4eec4965a8
zbjones <70654979+zbjones@users.noreply.github.com>
2022-01-05 10:39:16 -0600

    Added naive approach to calculate protein charge at a given pH
    Not implemented: various other pKa's, true isoelectric point determination (rather than charge at specific pH), proper implementation of probabilistic dissociation, values to json.

--- __init__.py
+++ __init__.py
@@ -9,7 +9,8 @@ import HETATM
 global current_fetch
 current_fetch = None
 
-#TODO Move **ALL HARDCODED DATA** into a json
+
+# TODO Move **ALL HARDCODED DATA** into a json
 
 class container:
     def __init__(self):
@@ -23,45 +24,56 @@ class container:
 global CONTAINER
 CONTAINER = container()
 
+
 def ClearAll():
     global CONTAINER
     CONTAINER = container()
+
 
 def SetFetch(fetch):
     global CONTAINER
     CONTAINER.current_fetch = fetch
 
+
 def GetFetch():
     global CONTAINER
     return CONTAINER.current_fetch
+
 
 def SetProteinChains(pchain):
     global CONTAINER
     CONTAINER.ProteinChains = pchain
 
+
 def GetProteinChains():
     global CONTAINER
     return CONTAINER.ProteinChains
+
 
 def SetDNAChains(dchain):
     global CONTAINER
     CONTAINER.DNAChains = dchain
 
+
 def GetDNAChains():
     global CONTAINER
     return CONTAINER.DNAChains
+
 
 def GetRNAChains():
     global CONTAINER
     return CONTAINER.RNAChains
 
+
 def SetRNAChains(rchain):
     global CONTAINER
     CONTAINER.RNAChains = rchain
 
+
 def GetHETATMChains():
     global CONTAINER
     return CONTAINER.HETATMChains
+
 
 def SetHETATMChains(hchain):
     global CONTAINER
@@ -78,10 +90,12 @@ def Fetch(prot):
     except urllib.error.URLError:
         sys.stderr.write("The requested pdb code could not be retrieved or does not exist\n")
 
-#Clears the fetched protein
+
+# Clears the fetched protein
 def ClearFetch():
     global CONTAINER
     CONTAINER.current_fetch = None
+
 
 # Wrapper for the ParsePDB file to allow functionality with a fetched protein
 def Parse():
@@ -91,6 +105,7 @@ def Parse():
         return ParsePDB(GetFetch().splitlines())
     except apalibExceptions.NoFetchError as e:
         sys.stderr.write(e.message)
+
 
 # TODO get this to work with DNA and RNA domains https://pdb101.rcsb.org/learn/guide-to-understanding-pdb-data/primary-sequences-and-the-pdb-format
 # Parsing DNA/RNA should be essentially the same as an amino acid atom-group, with some intricacies. 1pyi is a good example
@@ -104,7 +119,7 @@ def ParsePDB(pdbFile):
     HETATMChains = dict()
     for line in pdbFile:
         if line[0:4] == "ATOM" or line[0:6] == 'HETATM':
-            #Create a new chain if needed
+            # Create a new chain if needed
             chain = line[20:22].strip()
             if chain == '':
                 chain = '$'
@@ -116,9 +131,10 @@ def ParsePDB(pdbFile):
             occupancy = float(line[55:60])
             bfactor = float(line[60:67])
             element = line[77:].strip()
-            newAtom = Atom.Atom(number=atomNumber, coordinates=coordinates, id=id, occupancy=occupancy, b_factor=bfactor, element=element, residue=groupName)
+            newAtom = Atom.Atom(number=atomNumber, coordinates=coordinates, id=id, occupancy=occupancy,
+                                b_factor=bfactor, element=element, residue=groupName)
 
-            #RNA atom
+            # RNA atom
             if line[0:6] == "HETATM":
                 if chain not in HETATMChains:
                     HETATMChains[chain] = dict()
@@ -146,7 +162,7 @@ def ParsePDB(pdbFile):
                     proteinChains[chain][groupNumber] = AminoAcid.AminoAcid(name=groupName, number=groupNumber)
                 proteinChains[chain][groupNumber].InsertAtom(newAtom)
 
-    #Postparsing functions
+    # Postparsing functions
     SetProteinChains(proteinChains)
     SetDNAChains(DNAChains)
     SetRNAChains(RNAChains)
@@ -156,13 +172,81 @@ def ParsePDB(pdbFile):
         for index in GetProteinChains()[chain]:
             GetProteinChains()[chain][index].CalculateCentroid(GetProteinChains()[chain][index].atoms)
 
-#TODO in all GETTERS in each class, add a check to see if variable exists
-#if var in self.__dict__:
+
+#  Naive approach to calculating isoelectric point at a specific pH
+
+def getIEP(pHofInterest):
+    global CONTAINER
+    curr_protein_chains = CONTAINER.ProteinChains
+
+    # initialize dict of counts
+    total_counts = {'NTERM': 0, 'CTERM': 0, 'CYS': 0, 'ASP': 0, 'GLU': 0, 'HIS': 0, 'LYS': 0, 'ARG': 0, 'TYR': 0}
+
+    for key, value in curr_protein_chains.items():
+        current_chain = value
+        total_counts['NTERM'] += 1
+        total_counts['CTERM'] += 1
+
+        for key, value in current_chain.items():
+            if value.name == "CYS":
+                total_counts['CYS'] += 1
+            elif value.name == "ASP":
+                total_counts['ASP'] += 1
+            elif value.name == "GLU":
+                total_counts['GLU'] += 1
+            elif value.name == "HIS":
+                total_counts['HIS'] += 1
+            elif value.name == "LYS":
+                total_counts['LYS'] += 1
+            elif value.name == "ARG":
+                total_counts['ARG'] += 1
+            elif value.name == "TYR":
+                total_counts['TYR'] += 1
+
+    print(total_counts)
+
+    # TODO Add pKa values to JSON file
+    # using "EMBOSS" pKa's
+
+    sum_charges = 0
+
+    if pHofInterest < 8.6:
+        sum_charges += total_counts['NTERM']
+    if pHofInterest > 3.6:
+        sum_charges -= total_counts['CTERM']
+    if pHofInterest > 8.5:
+        sum_charges -= total_counts['CYS']
+    if pHofInterest > 3.9:
+        sum_charges -= total_counts['ASP']
+    if pHofInterest > 4.1:
+        sum_charges -= total_counts['GLU']
+    if pHofInterest < 6.5:
+        sum_charges += total_counts['HIS']
+    if pHofInterest < 10.8:
+        sum_charges += total_counts['LYS']
+    if pHofInterest < 12.5:
+        sum_charges += total_counts['ARG']
+    if pHofInterest > 10.1:
+        sum_charges -= total_counts['TYR']
+
+    return sum_charges
+
+
+
+
+
+
+
+
+
+
+# TODO in all GETTERS in each class, add a check to see if variable exists
+# if var in self.__dict__:
 #   return var
-#return None
+# return None
 
 
-#TODO Complete the below functions before pushing version 1.0.0
+# TODO Complete the below functions before pushing version 1.0.0
 
 # def SurfaceArea(**kwargs):
 #     if 'domain' not in kwargs:
@@ -206,14 +290,14 @@ def ParsePDB(pdbFile):
 # def CheckBridge(object1, object2):
 #     print("stub")
 
-#TODO Make sure ALL flags are properly cleared automatically
+# TODO Make sure ALL flags are properly cleared automatically
 
 
 ########################################################################################################################
 
-Fetch('5u59')
+Fetch('3mzw')
 Parse()
 print(GetProteinChains()['A'][1])
+print(getIEP(7.4))
 # print(GetProteinChains()['A'][1].GetAtoms()[0].GetCoordinates(), GetProteinChains()['A'][1].GetAtoms()[2].GetCoordinates())
 # print(GetDistance(GetProteinChains()['A'][1].GetAtoms()[0].GetCoordinates(), GetProteinChains()['A'][1].GetAtoms()[2].GetCoordinates()))
-
